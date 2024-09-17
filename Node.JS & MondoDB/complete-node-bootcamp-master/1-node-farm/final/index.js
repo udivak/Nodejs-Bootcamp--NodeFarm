92fcf5dda6a2a1aac2cf656fcee24c664cbbba8c
udivak <udivak9@gmail.com>
2024-09-17 15:30:09 +0300

    :)

--- Node.JS & MondoDB/complete-node-bootcamp-master/1-node-farm/final/index.js
+++ Node.JS & MondoDB/complete-node-bootcamp-master/1-node-farm/final/index.js
@@ -90,7 +90,6 @@ const server = http.createServer((req, res) => {
     res.end('<h1>Page not found!</h1>');
   }
 });
-
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
 });
